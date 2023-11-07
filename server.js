8d5d1133e894eeeedc3545a42407aecc5ccf2ef4
Alberto <74522184+albim360@users.noreply.github.com>
2023-11-07 15:17:31 +0100

    FEAT bonus

--- server.js
+++ server.js
@@ -3,15 +3,29 @@ const http = require("http");
 const dotenv = require("dotenv");
 dotenv.config();
 
+
 const port = +process.env.PORT || 3000;
+
+// Creo la costante per la visualizzazione del server
 const server = http.createServer(function (req, res) {
+    // Lo definisco come contenuto HTML
     res.writeHead(200, {'Content-Type': 'text/html'});
-    // res.end('<h1> ${process.env.TEXT} </h1>');
-    res.end(` <h1> ${process.env.TEXT} </h1>`);
+
+    // Definisco le parole random presenti nel .env come json
+    const paroleRandom = JSON.parse(process.env.RANDOMTEXT);
+    console.log(paroleRandom);
+    // Prendo l'index delle parole random
+    const randomIndex = Math.floor(Math.random() * paroleRandom.length);
+
+    // Seleziono una parola casuale dall'array
+    const randomText = paroleRandom[randomIndex];
+    // Stampo in pagina la parola casuale
+    res.end(` <h1> ${randomText} </h1>`);
 
 
 });
-
+// Starto il server
 server.listen(port, function() {
+    //Faccio sì che nel terminale esca direttamente il link clickabile per aprire il sito 
     console.log("Server is running on http://localhost:" + port)
 }); 
